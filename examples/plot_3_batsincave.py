eaeb8214dbcf8d0de98556b6ba98d746b0fbbedd
tbeleyur <tbeleyur@orn.mpg.de>
2022-03-13 19:49:54 +0100

    updated and revised version of transforming_research_code

--- examples/plot_3_batsincave.py
+++ examples/plot_3_batsincave.py
@@ -5,8 +5,6 @@ Matching and tracking bats in a cave
 Here let's use some more experimental data to find 2D correspondences, 
 and finally 3D paths for 2-3 bats seen flying together in a cave. 
 """
-
-
 import matplotlib.pyplot as plt 
 import mat73
 import numpy as np 
@@ -17,8 +15,8 @@ from track2trajectory.match2d import generate_2d_correspondences
 from track2trajectory.camera import Camera
 from track2trajectory.projection import project_to_2d
 from track2trajectory.synthetic_data import make_rotation_mat_fromworld, get_cam_coods
-from track2trajectory.dlt_to_world import transformation_matrix_from_dlt
-    
+from track2trajectory.dlt_to_world import transformation_matrix_from_dlt, cam_centre_from_dlt
+
 threecam_xy = pd.read_csv('DLTdv8_data_p000_15000_3camxypts.csv')
 
 def reformat_dltdv_frame(dltdv_df, camname, camnum):
@@ -103,17 +101,18 @@ def make_transformation_matrix_from_dlt(dltc):
     Tmat = make_rotation_mat_fromworld(shifted_rotmat1, t11[-1,:3])
     return Tmat
 
-
-
-# cam11 = Camera(1, [0,0,0], 526, c_x, c_y, f_x, f_y, Kteax, T1cam[:3,-1], T1cam[:3,:3],
-#               dist_coefs, [], Pcam2)
 T1cam = make_transformation_matrix_from_dlt(c1_dlt)
 T2cam = make_transformation_matrix_from_dlt(c2_dlt)
 T3cam = make_transformation_matrix_from_dlt(c3_dlt)
 
+cam1C = cam_centre_from_dlt(c1_dlt)
+cam2C = cam_centre_from_dlt(c2_dlt)
+cam3C = cam_centre_from_dlt(c3_dlt)
 
 #%% 
-# Generate camera objects 
+# Generate camera objects and also remember to get the relevant rotation and 
+# translation matrices in the camera->world frame. 
+
 
 c_x, c_y, f, f_x, f_y = 320, 256, 526, 526, 526
 cam1 = Camera(1, [0,0,0], 526, c_x, c_y, f_x, f_y, Kteax, T1cam[:3,-1], T1cam[:3,:3],
@@ -123,80 +122,29 @@ cam2 = Camera(2, [0,0,0], 526, c_x, c_y, f_x, f_y, Kteax, T2cam[:3,-1], T2cam[:3
 cam3 = Camera(3, [0,0,0], 526, c_x, c_y, f_x, f_y, Kteax, T3cam[:3,-1], T3cam[:3,:3],
               dist_coefs, [], Pcam3)
 
-F = calcFundamentalMatrix(cam1, cam2)
+F13 = calcFundamentalMatrix(cam1, cam3)
+F12 = calcFundamentalMatrix(cam1, cam2)
+F23 = calcFundamentalMatrix(cam2, cam3)
 #%%
 
-# threed_matches = match_2dpoints_to_3dtrajectories(cam1, cam3, 
-#                                                   cam1_xy,
-#                       
-
-
-twocam_matches, failed_2dmatches = generate_2d_correspondences(cam1, cam2,
-                                                               cam1_xy,
-                                                               cam2_xy, F,
-                                                               backpred_tol=5,
-                                                               match_method='3dbackpred') 
-print(twocam_matches)
-
-print(failed_2dmatches)
-# threed_matches = match_2dpoints_to_3dtrajectories(cam1, cam2, 
-#                                                   cam1_xy,
-#                                                   cam2_xy, F)
-#%%
-
-fnum = 0
-c1_byframe = cam1_xy.groupby('frame')
-c1_f0 = c1_byframe.get_group(fnum)
-c2_byframe = cam2_xy.groupby('frame')
-c2_f0 = c2_byframe.get_group(fnum)
-
-#%% 
-import cv2
-idx = 0
-pt1_homog = np.float32(cam1_xy.loc[idx,['x','y']].to_numpy().reshape(1,1,2))
-pt2_homog = np.float32(cam2_xy.loc[idx,['x','y']].to_numpy().reshape(1,1,2))
-position = cv2.triangulatePoints(Pcam1, Pcam2, pt1_homog, pt2_homog)
-xyz = cv2.convertPointsFromHomogeneous(position.T).flatten()
-print(xyz)
+threed_matches = match_2dpoints_to_3dtrajectories(cam1, cam2, 
+                                                  cam1_xy,
+                                                  cam2_xy, F12,match_method='3dbackpred')
 
 #%%
+# Compare the readings with the groundtruth known
 
-c_x, c_y, f, f_x, f_y = 320, 256, 526, 526, 526
+df = pd.read_csv('DLTdv8_data_p000_15000_3camxyzpts.csv')
 
-t11, z11, _ = transformation_matrix_from_dlt(c1_dlt)
-r11 = t11[:3,:3]
-trans11 = t11[-1,:3]
+pt3d_bycam = threed_matches.groupby('c1_oid')
+plt.figure()
+a0 = plt.subplot(111 ,projection='3d')
+a0.view_init(azim=112, elev=12)
+for names in ['cam1-pt1', 'cam1-pt2', 'cam1-pt3']:
+    p1 = pt3d_bycam.get_group(names)   
+    plt.plot(p1['x'],p1['z'],p1['y'],'-.',label=names)
 
-
-shifter_mat = np.row_stack(([1,0,0,0],
-                            [0,1,0,0],
-                            [0,0,-1,0],
-                            [0,0,0,1]))
-shifted_rotmat1 = np.matmul(t11, shifter_mat)[:3,:3]
-T1cam = make_rotation_mat_fromworld(shifted_rotmat1, t11[-1,:3])
-
-
-cam11 = Camera(1, [0,0,0], 526, c_x, c_y, f_x, f_y, Kteax, T1cam[:3,-1], T1cam[:3,:3],
-              dist_coefs, [], Pcam2)
-
-gt_df =  pd.DataFrame(data={'frame':[0],
-                            'oid':[1],
-                            'x':[xyz[0]],
-                            'y':[xyz[1]],
-                            'z':[xyz[2]]})
-xzy = np.float32([xyz[i] for i in [0,2,1]])
-r_m, _ = cv2.Rodrigues(cam11.r_mtrx)
-oi, _ = cv2.projectPoints(xzy, r_m,
-                          cam11.t_mtrx, 
-                          cam11.i_mtrx, 
-                          cam11.cof_mtrx)
-print('cv2 proj',oi, f'original: {pt1_homog}')
-
-kt = np.matmul(Kteax,T1cam[:3,:])
-out_homog = np.matmul(kt, np.append(xyz,1))
-print('my own calc',out_homog[:-1]/out_homog[-1])
-
-#print(projxy)
-#%%
-print(project_to_2d(xyz, cam11))
-
+for i,each in enumerate([cam1C, cam2C, cam3C]):
+    a,b,c = each
+    plt.plot(a,b,c,'^',label='cam'+str(i))
+plt.legend()
