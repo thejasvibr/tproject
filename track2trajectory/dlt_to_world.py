a3a07f9cb67a992d5256260c218b444abae7bdc5
tbeleyur <tbeleyur@orn.mpg.de>
2023-03-20 17:22:50 +0100

    Add partialdlt and dlt_inverse from DLTdv7 - these functions generate the epipolar lines for a point from Cam1 to Cam2.

--- track2trajectory/dlt_to_world.py
+++ track2trajectory/dlt_to_world.py
@@ -9,8 +9,8 @@ import pandas as pd
 
 def cam_centre_from_dlt(coefs):
     '''
-    
-    
+    Get the transformation from camera 3d to world 3d.
+
     Reference
     ---------
     * http://www.kwon3d.com/theory/dlt/dlt.html Equation 25
@@ -112,3 +112,93 @@ def assign_coefs_rowwise(coefs):
                 pass
     dlt_mat[-1,-1] = 0
     return dlt_mat
+
+
+def dlt_inverse(c, xyz):
+    '''
+    A Python port of the dlt_inverse MATLAB function by Ty Hedrick from 
+    DLTdv7
+    
+    Generates 2D pixels of a 3D point given the 3D xyz coordinates and 
+    camera DLT coefficients. 
+    
+    Parameters
+    ----------
+    c : (11,) np.array
+        11 DLT coefficients
+    xyz : (nframes, 3) np.array
+        N x 3d xyz coordinates 
+    
+    Returns
+    -------
+    uv : (nframes,2) np.array
+        row and col pixels across frames
+    '''
+    nrows = xyz.shape[0]
+    
+    uv = np.empty((nrows,2))
+    uv[:,:] = np.nan
+    uv_col0_numerator = xyz[:,0]*c[0] + xyz[:,1]*c[1] + xyz[:,2]*c[2] + c[3]
+    uv_col0_denom = xyz[:,0]*c[8]+xyz[:,1]*c[9]+xyz[:,2]*c[10]+1
+    uv[:,0] = uv_col0_numerator/uv_col0_denom
+    
+    uv_col1_num = xyz[:,0]*c[4]+xyz[:,1]*c[5]+xyz[:,2]*c[6]+c[7]
+    uv_col1_denom = xyz[:,0]*c[8]+xyz[:,1]*c[9]+xyz[:,2]*c[10]+1
+    uv[:,1] = uv_col1_num/uv_col1_denom
+    return uv
+    
+
+def partialdlt(u,v,C1, C2):
+    '''
+    A Python port of the partialdlt MATLAB function by Ty Hedrick from DLTdv7
+    
+    Parameters
+    ----------
+    u, v: floats
+        x,y coods of a point in cam 1
+    C1, C2 : (11,) np.array
+        Array with 11 DLT coefficients 
+    Returns
+    -------
+    m : float
+        Slope of the epipolar line 
+    b : float
+        Y -intercept of the epipolar line on cam2 
+
+    '''
+        
+   
+    # pick 2 random Z (actual values are not important)
+    z = np.array([500, -500])
+    
+    # for each z predict x & y
+    x = np.tile(np.nan, 2);
+    y = np.tile(np.nan, 2);
+    for i in range(2):
+      Z = z[i]
+      pt1 = u*C1[8]*C1[6]*Z + u*C1[8]*C1[7] - u*C1[10]*Z*C1[4] -u*C1[4]
+      pt2 = C1[0]*v*C1[10]*Z + C1[0]*v - C1[0]*C1[6]*Z - C1[0]*C1[7]
+      pt3 = -C1[2]*Z*v*C1[8] + C1[2]*Z*C1[4] - C1[3]*v*C1[8] + C1[3]*C1[4]
+      denom =   u*C1[8]*C1[5] - u*C1[9]*C1[4] + C1[0]*v*C1[9] - C1[0]*C1[5] -C1[1]*v*C1[8] + C1[1]*C1[4]
+      y[i]= (pt1 + pt2 + pt3 )/denom
+      y[i] *= -1
+
+      Y = y[i]
+      
+      x[i]= (v*C1[9]*Y+v*C1[10]*Z+v-C1[5]*Y-C1[6]*Z-C1[7])/(v*C1[8]-C1[4])
+      x[i] *= -1 
+    
+    # back the points into the cam2 X,Y domain
+    xy = np.zeros((2,2));
+    for i in range(2):
+      xy[i,:]=dlt_inverse(C2,np.array([x[i],y[i],z[i]]).reshape(-1,3))
+    
+    # get a line equation back, y=mx+b
+    m = (xy[1,1]-xy[0,1])/(xy[1,0]-xy[0,0]);
+    b = xy[0,1] - m*xy[0,0];
+    return m, b
+    
+
+
+
+
